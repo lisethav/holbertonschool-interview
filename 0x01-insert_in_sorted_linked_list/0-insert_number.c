deccdb36a01be92ece7969228f76251755fba1c5
lisethav <lisethav@gmail.com>
2022-05-23 23:18:58 -0500

    linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * insert_node - function in C that inserts a number into a sorted singly linked list
+ * @head: head of the list
+ * @number: number to be inserted
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp = NULL;
+	listint_t *node = NULL;
+
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		node->next = NULL;
+		return (node);
+	}
+
+	tmp = *(head);
+	if (tmp->n >= number)
+	{
+		node->next = tmp;
+		*(head) = node;
+		return (node);
+	}
+	while (tmp->next != NULL) {
+		if (tmp->next->n >= number)
+		{
+			node->next = tmp->next;
+			tmp->next = node;
+			return (node);
+		}
+		tmp = tmp->next;
+	}
+	tmp->next = node;
+	node->next = NULL;
+	return (node);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,2 @@
+
+0x01. Insert in sorted linked list
