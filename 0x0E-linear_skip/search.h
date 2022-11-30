9eaaa09e48889328c0065139777605ec0c35d5ac
lisethav <lisethav@gmail.com>
2022-11-30 16:55:08 -0500

    library

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -1,6 +1,12 @@
 #ifndef _SEARCH_H_
 #define _SEARCH_H_
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
@@ -14,12 +20,15 @@
  */
 typedef struct skiplist_s
 {
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
 } skiplist_t;
 
-skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *head, int value);
 
 #endif /* _SEARCH_H_ */
