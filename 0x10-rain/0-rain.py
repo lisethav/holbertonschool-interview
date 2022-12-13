258eaa8aa66431206bde5593b0f0caac8bf4b111
lisethav <lisethav@gmail.com>
2022-12-12 23:39:42 -0500

    all

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module for peak list rainfall problem """
+
+
+def rain(walls):
+    """ Calculates how much water retained """
+
+    if not walls or len(walls) <= 2:
+        return 0
+
+    list_len = len(walls)
+    total = 0
+    for idx, elem in enumerate(walls):
+        next = idx + 1
+        if elem <= next:
+            if next == 0:
+                total += 1
+            else:
+                total += elem
+    return 
