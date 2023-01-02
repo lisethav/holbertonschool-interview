f8866b0d43e20e21f0edf4e3282202bfa1c00adf
lisethav <lisethav@gmail.com>
2023-01-01 23:16:01 -0500

    fix one

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,34 +1,55 @@
 #!/usr/bin/python3
+""" Queries Reddit API recursively for all hot post titles from subreddit
+    Returns None if invalid subreddit
 """
-Count parse hot topics Reddit Api
-"""
+import requests as r
+import requests.auth as ra
+from sys import argv
 
 
-import requests
-
-
-def count_words(subreddit, word_list):
-    """ count words
+def count_words(subreddit, hot_list=[], after=None):
+    """ Recursive query system for Reddit API hot posts
     """
-    base_url = 'https://www.reddit.com'
-    payload = {'q': word_list[0], 'sort': 'relevance'}
-    response = requests.get(
-        base_url + '/r/' + subreddit + '/hot.json',
-        headers={'User-Agent': 'Custom user'},
-        params=payload
-    )
 
-    """print(response.status_code)
-    print(response.text)
-"""
-    values = response.json()
-    count = 0
+    # Generates token & bearer for requests to oauth.reddit.com endpoints
+    # https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example
 
-    for i in range(len(values['data']['children'])):
-        count += 1
+    client_id = "i_m3aNLQE0vskA"
+    secret = "UykV3Qz2TGSWSWaMmSeOWgR5JT4"
+    my_username = "causticpop"
+    my_pwd = "@Holberton98"
 
-    word_count = {x: count for x in word_list}
-    for key, value in word_count.items():
-        print("{} : {}".format(key, value))
+    client_auth = ra.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password", "username":
+                 my_username, "password": my_pwd}
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
+    response = r.post("https://www.reddit.com/api/v1/access_token",
+                      auth=client_auth, data=post_data, headers=headers)
+    token = response.json().get("access_token")
+    bearer = response.json().get("token_type")
 
-    return word_count
+    # Queries oauth.reddit.com endpoints using token & bearer
+    # Sets limit on number of items returned in params variable
+
+    sub = subreddit
+    sub_url = "https://oauth.reddit.com/r/{}/hot".format(sub)
+
+    headers = {"Authorization": "{} {}".format(bearer, token),
+               "User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
+    params = {'limit': 100, 'after': after}
+    response = r.get(sub_url, headers=headers, params=params)
+
+    # handles error response; invalid subreddit
+    if response.status_code is not 200:
+        return None
+
+    # peels the onion of nested dicts and lists
+    else:
+        response_json = response.json().get('data').get('children')
+        for subdict in response_json:
+            hot_list.append(subdict.get('data').get('title'))
+        if response.json().get('data').get('after') is not None:
+            after = response.json().get('data').get('after')
+            return count_words(subreddit, hot_list, after)
+        else:
+            return hot_list
